1cf38e8db1fec8015b697bf81a25bd38a0361ada
LPenjin <l.penjo@gmail.com>
2022-11-09 12:48:49 +0100

    Adds Simulator to lab

---
+++ 2. labos/zad3-simulator/Primjeri/Python/primjer.py
@@ -0,0 +1,10 @@
+import sys
+
+while True:
+    str = raw_input()
+    (L,D,LK,DK,V,S) = [int(s) for s in str.split(" ") if s.isdigit()]
+
+    akcel = 5
+    kormilo = 5
+    print(akcel, kormilo)
+    sys.stdout.flush()
